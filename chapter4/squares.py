7aeb9b2a281a56bbd01729ede2c242f32a14e7a8
Dylan J <ebumping@gmail.com>
2022-06-04 23:48:36 -0400

    More squares

--- chapter4/squares.py
+++ chapter4/squares.py
@@ -1,7 +1,10 @@
-squares = []
-for value in range(1,11):
-    square = value ** 2
-    squares.append(square)
+#squares = []
+#for value in range(1,11):
+#    square = value ** 2
+#    squares.append(square)
 
+#print(squares)
+#print(sum(squares))
+
+squares = [value**2 for value in range(1,11)]
 print(squares)
-print(sum(squares))
