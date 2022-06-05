4fdecdc7278896e1dba9764cd9ad51d0f2a1522a
Dylan J <ebumping@gmail.com>
2022-06-05 01:24:59 -0400

    Added alien.py

---
+++ chapter6/alien.py
@@ -0,0 +1,19 @@
+#alien_0 = {'color': 'green', 'points': 5}
+#print(alien_0)
+
+#alien_0['x_position'] = 0
+#alien_0['y_position'] = 25
+#print(alien_0)
+
+#alien_0 = {}
+
+#alien_0['color'] = 'green'
+#alien_0['points'] = 5
+
+#print(alien_0)
+
+alien_0 = {'color': 'green'}
+print(f"The alien is now {alien_0['color']}.")
+
+alien_0['color'] = 'yellow'
+print(f"The alien is now {alien_0['color']}.")
