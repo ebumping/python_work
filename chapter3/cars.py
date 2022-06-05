6032afaf1575e0153ba5cb9ef92e6ba2beec9f38
Dylan J <ebumping@gmail.com>
2022-06-04 21:29:33 -0400

    Added cars.py

---
+++ chapter3/cars.py
@@ -0,0 +1,9 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+#cars.sort()
+#print(cars)
+#cars.sort(reverse=True)
+#print(cars)
+print(cars)
+sorted = sorted(cars)
+print(f"Sorted...{sorted}")
+print(cars)
