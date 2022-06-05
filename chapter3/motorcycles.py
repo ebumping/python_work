b7cea9fd256ce19808c1e03c000e111c281df0a2
Dylan J <ebumping@gmail.com>
2022-06-04 21:14:26 -0400

    More on motorcycles.py

--- chapter3/motorcycles.py
+++ chapter3/motorcycles.py
@@ -6,7 +6,11 @@ motorcycles.insert(1, 'harley')
 print(motorcycles)
 del motorcycles[1]
 print(motorcycles)
-popped_motorcycle = motorcycles.pop()
-print(popped_motorcycle)
+#popped_motorcycle = motorcycles.pop()
+#print(popped_motorcycle)
 first_owned = motorcycles.pop(0)
 print(f"The first motorcycle I owned was a {first_owned.title()}")
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print(f"\nA {too_expensive.title()} is too expensive for me.")
