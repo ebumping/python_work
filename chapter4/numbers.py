fb137678bcb4143c94d85447af5e3cb7ddcac706
Dylan J <ebumping@gmail.com>
2022-06-04 22:28:06 -0400

    added numbers.py, squares.py, fixed motorcycles

--- chapter4/numbers.py
+++ chapter4/numbers.py
@@ -4,9 +4,3 @@ for value in range(1,6):
 even_numbers = list(range(2, 11, 2))
 print(even_numbers)
 
-squares = []
-for value in range(1,11):
-    square = value ** 2
-    squares.append(square)
-
-print(squares)
