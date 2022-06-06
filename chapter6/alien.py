8b8e960e5ef803a7457bba297b5fb101ead12c9d
Dylan J <ebumping@gmail.com>
2022-06-05 20:48:55 -0400

    Finished chapter6

--- chapter6/alien.py
+++ chapter6/alien.py
@@ -12,8 +12,12 @@
 
 #print(alien_0)
 
-alien_0 = {'color': 'green'}
+alien_0 = {'color': 'green', 'points': 5}
 print(f"The alien is now {alien_0['color']}.")
 
 alien_0['color'] = 'yellow'
 print(f"The alien is now {alien_0['color']}.")
+
+print(alien_0)
+del alien_0['points']
+print(alien_0)
