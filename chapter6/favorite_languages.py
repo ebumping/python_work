c3435c47b67fd635f80ee19d9aa5325d42851c3e
Dylan J <ebumping@gmail.com>
2022-06-06 18:42:28 -0400

    Changed to alien and favorite_languages

--- chapter6/favorite_languages.py
+++ chapter6/favorite_languages.py
@@ -23,5 +23,12 @@ for name in favorite_languages.keys():
 if 'erin' not in favorite_languages.keys():
     print("Erin, please take our poll!")
 
+print("The following languages have been mentioned:")
+for language in favorite_languages.values():
+    print(language.title())
+
 for name in sorted(favorite_languages.keys()):
     print(f"{name.title()}, thank you for taking the poll.")
+
+for language in set(favorite_languages.values()):
+    print(language.title())
