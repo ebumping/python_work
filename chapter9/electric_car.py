bca867061b90c5bef1958fcc7e72d6c0e0de9f69
Dylan J <ebumping@gmail.com>
2022-06-16 00:02:03 -0400

    Modified cars and added electric_car

--- chapter9/electric_car.py
+++ chapter9/electric_car.py
@@ -27,19 +27,24 @@ class Car:
     def increment_odometer(self, miles):
         """Add the given amount to the odometer reading."""
         self.odometer_reading += miles
+class Battery:
+    """A simple attempt to model a battery for an electric car."""
+    def __init__(self, battery_size = 75):
+        """Initialize the battery's attributes."""
+        self.battery_size = battery_size
 
-    
-my_new_car = Car('audi', 'a4', '2019')
-print(my_new_car.get_descriptive_name())
-my_new_car.update_odometer(23)
-my_new_car.read_odometer()
-my_new_car
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}--kWh battery.")
+        
+class ElectricCar(Car):
+    """Represents aspects of a car, specific to electric vehicles"""
 
-my_used_car = Car('mazda', '6', 2008)
-print(my_used_car.get_descriptive_name())
+    def __init__(self, make, model, year):
+        """Initialize attributes of the parent class."""
+        super().__init__(make, model, year)
+        self.battery = Battery()
 
-my_used_car.update_odometer(179_500)
-my_used_car.read_odometer()
-
-my_used_car.increment_odometer(135)
-my_used_car.read_odometer()
+my_tesla = ElectricCar('tesla', 'model s', 2019)
+print(my_tesla.get_descriptive_name())
+my_tesla.battery.describe_battery()
